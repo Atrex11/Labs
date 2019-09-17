7421ee72b2c38cbc857f767a02306584f78d8137
Atrex11 <jsfei069@uottawa.ca>
2019-09-17 16:34:24 -0400

    add Information

--- Lab1_Git/PersonInfo1.java
+++ Lab1_Git/PersonInfo1.java
@@ -1,7 +1,7 @@
 public class PersonInfo1 {
 
-    public static String firstName = "Leisha";
-    public static String lastName = "Baker";
+    public static String firstName = "Jean";
+    public static String lastName = "sfeir";
     public static int studentNo = 338299;
     public static String description = "Likes to take pictures";
 
