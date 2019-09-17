4653b3ff3f94ab408b2685b050f69e0afba02ab2
Assaad Howayek <ahowa022@uottawa.ca>
2019-09-17 17:23:44 -0400

    Updated info on PersonInfo4

--- Lab1_Git/PersonInfo4.java
+++ Lab1_Git/PersonInfo4.java
@@ -1,9 +1,9 @@
 public class PersonInfo4 {
 
-    public static String firstName = "Alex";
-    public static String lastName = "Moujot";
-    public static int studentNo = 88499;
-    public static String description = "les ananas";
+    public static String firstName = "Assaad";
+    public static String lastName = "Howayek";
+    public static int studentNo = 300057716;
+    public static String description = "J'aime le basketball";
     
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
