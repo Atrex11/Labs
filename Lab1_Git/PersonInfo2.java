4f80f8b546da7ba4c2c16d3b1139184d2c1d6053
Elie Arja <earja087@uottawa.ca>
2019-09-17 17:23:11 -0400

    Commit in newBranch

--- Lab1_Git/PersonInfo2.java
+++ Lab1_Git/PersonInfo2.java
@@ -3,7 +3,7 @@ public class PersonInfo2 {
     public static String firstName = "Elie";
     public static String lastName = "Arja";
     public static int studentNo = 60782;
-    public static String description = "MacBook";
+    public static String description = "Update 2 to test new branch";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
