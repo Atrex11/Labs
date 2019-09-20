14083eb36e0237cdf27f7b39d16aeeaa02699a64
Atrex11 <jsfei069@uottawa.ca>
2019-09-20 09:18:16 -0400

    Updated Information

--- Lab1_Git/PersonInfo1.java
+++ Lab1_Git/PersonInfo1.java
@@ -3,7 +3,7 @@ public class PersonInfo1 {
     public static String firstName = "Jean";
     public static String lastName = "Sfeir";
     public static int studentNo = 338299;
-    public static String description = "Likes to take pictures";
+    public static String description = "Likes to take pictures and something ";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
